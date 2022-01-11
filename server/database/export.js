85fc9190a319e8c61165b138770d8e2126eff0cc
Ugo <ugo.prenat@gmail.com>
2022-01-11 10:52:26 +0100

    first files

--- server/database/export.js
+++ server/database/export.js
@@ -1,15 +1,10 @@
 const connect = require('./connection')
 const models = require('./models/export.models')
 
-const accountController = require('../controller/account.controller')
-
 module.exports = {
     connect,
-
     accounts: models.account,
     message: models.message,
     rewards: models.rewards,
     todoList: models.todoList,
-
-    accountController
 }
