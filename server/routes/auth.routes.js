213aa76cfd6f70c6d94fa6a4e1f5b4120fc9c782
Léo <leo.gregori95@gmail.com>
2022-03-22 16:34:06 +0100

    Auto stash before merge of "master" and "origin/master"

--- server/routes/auth.routes.js
+++ server/routes/auth.routes.js
@@ -1,43 +1,17 @@
-const express = require('express')
-const router = express.Router()
-const bcrypt = require('bcrypt')
+const express = require("express");
+const router = express.Router();
+const bcrypt = require("bcrypt");
 
-const authenticateToken = require('../middleware/token/authenticateToken')
+const authenticateToken = require("../middleware/token/authenticateToken");
 
-const authController = require('../controllers/auth/export')
+const authController = require("../controllers/auth/export");
 
+router.get("/", authenticateToken, (req, res) => {
+  // If the autehticateToken middle doens't return an error, return the user
+  res.status(200).send({ status: 200, user: req.user });
+});
+router.post("/login", authController.login);
+router.post("/register", authController.register);
+router.post("/reset/password", authController.resetPassword);
 
-router.get('/', authenticateToken, (req, res) => {
-    // If the autehticateToken middle doens't return an error, return the user
-    res.status(200).send({ status: 200, user: req.user })
-})
-router.post('/login', authController.login)
-router.post('/register', authController.register)
-router.post('/reset/password', authController.resetPassword)
-
-            res.status(200).send({ code: 200, accessToken })
-        })
-        .catch((error) => res.status(400).send({ code: 400, message: error }))
-    }
-})
-
-
-
-
-
-
-
-
-async function isUsernameExists(username) {
-    // Check if the given username already exists or not
-    const user = await Accounts.findOne({ username }).then(res => { return res })
-    
-    if (user) return true
-    else return false
-}
-function generateAccessToken(user) {
-    // Generate a Json Web Token for a specific user
-    return jwt.sign(user.toJSON(), process.env.ACCESS_TOKEN_SECRET)
-}
-
-module.exports = router
+module.exports = router;
